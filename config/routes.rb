5e230a7a2184c770dc622a2712f286c434237d39
tasuku-lab280 <loweng280@gmail.com>
2021-06-05 03:04:33 +0900

    webpacker疎通

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :front do
+    root 'home#index' # トップ
+  end
 end
---
+++ frontend/packs/application.js
@@ -0,0 +1,9 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+
+import '../src/style.scss';
+
+console.log('Hello Webpacker!');
