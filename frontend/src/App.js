217a9de32fa0a10e7b832952758e3382ed0b7a82
tasuku-lab280 <loweng280@gmail.com>
2021-07-22 22:26:39 +0900

    ヘッダー

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import Router from "./Router";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
-    <Router />
+    <>
+      <Header />
+      <Router />
+    </>
   );
 }
 
