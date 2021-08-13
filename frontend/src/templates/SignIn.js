6189a201200f997e7aa2ebb455eedd4e58920ce0
tasuku-lab280 <loweng280@gmail.com>
2021-08-13 11:02:32 +0900

    ログイン画面実装

--- frontend/src/templates/SignIn.js
+++ frontend/src/templates/SignIn.js
@@ -1,6 +1,145 @@
+import { useState, useContext } from "react"
+import { useHistory, Link } from "react-router-dom"
+import Cookies from "js-cookie"
+
+import { makeStyles } from "@material-ui/core/styles"
+import { Typography } from "@material-ui/core"
+import TextField from "@material-ui/core/TextField"
+import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+import CardHeader from "@material-ui/core/CardHeader"
+import Button from "@material-ui/core/Button"
+import Box from "@material-ui/core/Box"
+
+import { AuthContext } from "../Router";
+import AlertMessage from "../components/utils/AlertMessage";
+import { signIn } from "../lib/api/auth";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(6)
+  },
+  submitBtn: {
+    marginTop: theme.spacing(2),
+    flexGrow: 1,
+    textTransform: "none"
+  },
+  header: {
+    textAlign: "center"
+  },
+  card: {
+    padding: theme.spacing(2),
+    maxWidth: 400
+  },
+  box: {
+    marginTop: "2rem"
+  },
+  link: {
+    textDecoration: "none"
+  }
+}))
+
 const SignIn = () => {
+  const classes = useStyles()
+  const history = useHistory()
+
+  const { setIsSignedIn, setCurrentUser } = useContext(AuthContext)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [alertMessageOpen, setAlertMessageOpen] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const params = {
+      email: email,
+      password: password
+    }
+
+    try {
+      const res = await signIn(params)
+      console.log(res)
+
+      if (res.status === 200) {
+        // ログインに成功した場合はCookieに各値を格納
+        Cookies.set("_access_token", res.headers["access-token"])
+        Cookies.set("_client", res.headers["client"])
+        Cookies.set("_uid", res.headers["uid"])
+
+        setIsSignedIn(true)
+        setCurrentUser(res.data.data)
+
+        history.push("/")
+
+        console.log('ログインに成功しました。')
+      } else {
+        setAlertMessageOpen(true)
+      }
+    } catch (err) {
+      console.log(err)
+      console.log(err.response)
+      setAlertMessageOpen(true)
+    }
+  }
+
   return (
-    <div>ログイン</div>
+    <>
+      <form noValidate autoComplete="off">
+        <Card className={classes.card}>
+          <CardHeader className={classes.header} title="ログイン" />
+          <CardContent>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              label="Eメール"
+              value={email}
+              margin="dense"
+              onChange={event => setEmail(event.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              label="パスワード"
+              type="password"
+              placeholder="At least 6 characters"
+              value={password}
+              margin="dense"
+              autoComplete="current-password"
+              onChange={event => setPassword(event.target.value)}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              fullWidth
+              color="default"
+              disabled={!email || !password ? true : false} // 空欄があった場合はボタンを押せないように
+              className={classes.submitBtn}
+              onClick={handleSubmit}
+            >
+              ログインする
+            </Button>
+            <Box textAlign="center" className={classes.box}>
+              <Typography variant="body2">
+                Don't have an account? &nbsp;
+                <Link to="/signup" className={classes.link}>
+                  Sign Up now!
+                </Link>
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </form>
+      <AlertMessage // エラーが発生した場合はアラートを表示
+        open={alertMessageOpen}
+        setOpen={setAlertMessageOpen}
+        severity="error"
+        message="Invalid emai or password"
+      />
+    </>
   )
 }
 
